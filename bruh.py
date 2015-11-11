ac1cc8def238230eff80eec36e45f6249d0fe48b
t0pth4t <t0pth4t@yahoo.com>
2015-11-10 23:11:12 -0600

    new and improved bruh

--- bruh.py
+++ bruh.py
@@ -1,7 +1,6 @@
 from kivy.uix.boxlayout import BoxLayout
 from kivy.app import App
 from kivy.uix.button import Button
-from functools import partial
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
@@ -11,114 +10,88 @@ class DemoBox(BoxLayout):
     def __init__(self, **kwargs):
         super(DemoBox, self).__init__(**kwargs)
         self.orientation = "vertical"
-        self.buckets = "0"
+        self.buckets = 0
         self.garbage = 0
         self.funct = 0
-        self.tracker = TextInput(text=self.buckets)
+        self.tracker = TextInput(text=str(self.buckets))
         self.tracker.size_hint = (1, .1)
         self.add_widget(self.tracker)
         buttons = GridLayout(cols=4)
         self.add_widget(buttons)
-        bt1 = Button(text="0")
-        bt2 = Button(text="1")
-        bt3 = Button(text="2")
-        bt4 = Button(text="3")
-        bt5 = Button(text="4")
-        bt6 = Button(text="5")
-        bt7 = Button(text="6")
-        bt8 = Button(text="7")
-        bt9 = Button(text="8")
-        bt10 = Button(text="9")
-        bt11 = Button(text="+")
-        bt12 = Button(text="-")
-        bt13 = Button(text="=")
-        bt14 = Button(text="*")
-        bt15 = Button(text="/")
-        bt13.bind(on_press=self.numclear)
-        for but in [bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9, bt10, bt11, bt12, bt13, bt14, bt15]:
-            but.bind(on_press=self.trackerupdate)
-        bt1.bind(on_press=self.press0)
-        bt2.bind(on_press=self.press1)
-        bt3.bind(on_press=self.press2)
-        bt4.bind(on_press=self.press3)
-        bt5.bind(on_press=self.press4)
-        bt6.bind(on_press=self.press5)
-        bt7.bind(on_press=self.press6)
-        bt8.bind(on_press=self.press7)
-        bt9.bind(on_press=self.press8)
-        bt10.bind(on_press=self.press9)
-        bt11.bind(on_press=self.adder)
-        bt12.bind(on_press=self.minuser)
-        bt13.bind(on_press=self.equals)
-        bt14.bind(on_press=self.multiplyer)
-        bt15.bind(on_press=self.divider)
-        for but in [bt1, bt2, bt3, bt4, bt5, bt6, bt7, bt8, bt9, bt10, bt11, bt12, bt13, bt14, bt15]:
-            buttons.add_widget(but)
+        butts = []
+        for i in range(10):
+            butts.append(str(i))
+        butts.append("+")
+        butts.append("-")
+        butts.append("*")
+        butts.append("/")
+        butts.append("=")
+        butts.append("clear")
+        for butt in butts:
+            b = Button(text=str(butt))
+            b.bind(on_press=self.press)
+            buttons.add_widget(b)
+
+        self.symbolFunctions = {
+            "+":self.adder,
+            "-":self.minuser,
+            "=":self.equals,
+            "*":self.multiplyer,
+            "/":self.divider,
+            "clear":self.numclear
+        }
+
     def divider(self,obj):
         self.buckets = int(self.buckets)
         self.garbage = self.buckets
-        self.buckets = "0"
+        self.buckets = 0
         self.funct = 4
     def multiplyer(self,obj):
         self.buckets = int(self.buckets)
         self.garbage = self.buckets
-        self.buckets = "0"
+        self.buckets = 0
         self.funct = 3
     def equals(self,obj):
-        if self.funct == 1:
-            self.buckets = int(self.buckets)
-            self.buckets += self.garbage
-            print(self.buckets)
-        elif self.funct == 2:
-            self.buckets = int(self.buckets)
-            self.buckets = self.garbage - self.buckets
-            print(self.buckets)
-        elif self.funct == 3:
-            self.buckets = int(self.buckets)
-            self.buckets = self.garbage * self.buckets
-            print(self.buckets)
-        elif self.funct == 4:
-            self.buckets = int(self.buckets)
-            self.buckets = self.garbage / self.buckets
-            self.buckets = float(self.buckets)
-            print(self.buckets)
+
+        try:
+            buckets = int(self.buckets)
+            garbage = int(self.garbage)
+            if self.funct == 1:
+                self.buckets = buckets +  garbage
+            elif self.funct == 2:
+                self.buckets = garbage - buckets
+            elif self.funct == 3:
+                self.buckets = garbage * buckets
+            elif self.funct == 4:
+                self.buckets = garbage / buckets
+                self.buckets = float(self.buckets)
+        except (ValueError, ZeroDivisionError) as e:
+            print e
+            self.buckets = 0
+
         
     def adder(self, obj):
         self.buckets = int(self.buckets)
         self.garbage = self.buckets
-        self.buckets = "0"
+        self.buckets = 0
         self.funct = 1
     def minuser(self,obj):
         self.buckets = int(self.buckets)
         self.garbage = self.buckets
-        self.buckets = "0"
+        self.buckets = 0
         self.funct = 2
-    def printer(self, obj):
-        print(self.buckets)
-    def trackerupdate(self, obj):
-        self.tracker.text = str(float(self.buckets))
     def numclear(self, obj):
-        self.buckets = "0"
-    def press0(self, obj):
-        self.buckets += "0"
-    def press1(self, obj):
-        self.buckets += "1"
-    def press2(self, obj):
-        self.buckets += "2"
-    def press3(self, obj):
-        self.buckets += "3"
-    def press4(self, obj):
-        self.buckets += "4"
-    def press5(self, obj):
-        self.buckets += "5"
-    def press6(self, obj):
-        self.buckets += "6"
-    def press7(self, obj):
-        self.buckets += "7"
-    def press8(self, obj):
-        self.buckets += "8"
-    def press9(self, obj):
-        self.buckets += "9"
+        self.buckets = 0
+    def press(self,obj):
+        try:
+            int(obj.text)
+            self.buckets = str(self.buckets) + str(obj.text)
+            self.tracker.text = str(float(self.buckets))
+        except ValueError:
+            self.symbolFunctions[obj.text](obj)
+            self.tracker.text = str(float(self.buckets))
+
+
     def on_property(self, obj, value):
         print("Typical property change from", obj, "to", value)
 
